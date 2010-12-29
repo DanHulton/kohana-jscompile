853dc78f14c87706a9fc0ef7340a6ee63b36775d
Dan Hulton <dan@danhulton.com>
2010-12-29 12:12:32 -0500

    Updating with latest fixes from project.

--- config/jscompile.php
+++ config/jscompile.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-	'compress' => false,
-	'path'     => '', // Path relative to docroot to store compiled files
+	'compress' => false,    // SLOW - only use true when deployed
+	'path'     => '',       // Path relative to docroot to store compiled files
 );
--- init.php
+++ init.php
@@ -1,3 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
+$jscompile_cache_path = DOCROOT . Kohana::config('jscompile.path');
+
+if ( ! is_writable($jscompile_cache_path)) {
+    throw new Kohana_Exception('Directory :dir must be writable',
+        array(':dir' => Kohana::debug_path($jscompile_cache_path)));
+}
+
 require_once 'vendor/jsmin.php';
